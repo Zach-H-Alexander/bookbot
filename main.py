9cc9a40d1ae2842d3307ba97c7ff5743638ebb83
Dixie <ironspud@gmail.com>
2025-01-09 21:57:37 -0500

    updated main.py, need to break into functions

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("hello world")
+
+def main():
+	with open("/home/ironspud/workspace/github.com/Zach-H-Alexander/bookbot/books/frankenstein.txt") as f:
+		file_contents = f.read().lower()
+		
+		words = file_contents.split()
+		word_count = len(words)
+		letter_count = {}
+		for word in words:
+			for letter in word:
+				if letter in letter_count:
+					letter_count[letter] += 1
+				else:
+					letter_count[letter] = 1
+		print(letter_count)	
+	print(word_count)
+main()
