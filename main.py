2e959a5e68fc5c64d1c7b4bd11b0fe84cbe050e8
Dixie <ironspud@gmail.com>
2025-01-10 14:30:13 -0500

    bare bones bot!

--- main.py
+++ main.py
@@ -1,17 +1,37 @@
 
 def main():
+	import string
 	with open("/home/ironspud/workspace/github.com/Zach-H-Alexander/bookbot/books/frankenstein.txt") as f:
 		file_contents = f.read().lower()
 		
 		words = file_contents.split()
 		word_count = len(words)
 		letter_count = {}
-		for word in words:
+		letter_list = []
+		for word in words: 
 			for letter in word:
-				if letter in letter_count:
-					letter_count[letter] += 1
-				else:
-					letter_count[letter] = 1
-		print(letter_count)	
-	print(word_count)
+				if letter.isalpha():
+					if letter in letter_count:
+						letter_count[letter] += 1
+					else:
+						letter_count[letter] = 1
+	def sort_letters():
+		for letter in letter_count:
+			letter_list.append({"LETTER": letter, "COUNT": letter_count[letter]})
+	sort_letters()
+	def sort_on(dict):
+		return dict["COUNT"]
+	letter_list.sort(reverse=True, key=sort_on)
+
+	def print_report():
+		print("--Begin Report--")
+		print(f"{word_count} in doc")
+		for letter_dict in letter_list:
+			character =  letter_dict['LETTER']
+			times = letter_dict['COUNT']
+			print(f"The {character} appears {times} times!")
+		print("--End Report--")
+
+	print_report()
+
 main()
