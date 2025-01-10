3978f1a3490e93109621df16dd502a305db39034
Dixie <ironspud@gmail.com>
2025-01-10 14:53:10 -0500

    Cleaned it up a bit!

--- main.py
+++ main.py
@@ -1,37 +1,41 @@
+def sort_on(dict):
+        return dict["COUNT"]
+        letter_list.sort(reverse=True, key=sort_on)
 
 def main():
 	import string
-	with open("/home/ironspud/workspace/github.com/Zach-H-Alexander/bookbot/books/frankenstein.txt") as f:
+	path = input("Enter the file location of your book: ")
+
+	#Open file, process and lowercase all of it.
+	with open(path) as f:
 		file_contents = f.read().lower()
-		
 		words = file_contents.split()
-		word_count = len(words)
-		letter_count = {}
-		letter_list = []
-		for word in words: 
-			for letter in word:
-				if letter.isalpha():
-					if letter in letter_count:
-						letter_count[letter] += 1
-					else:
-						letter_count[letter] = 1
-	def sort_letters():
-		for letter in letter_count:
-			letter_list.append({"LETTER": letter, "COUNT": letter_count[letter]})
-	sort_letters()
-	def sort_on(dict):
-		return dict["COUNT"]
-	letter_list.sort(reverse=True, key=sort_on)
 
-	def print_report():
-		print("--Begin Report--")
-		print(f"{word_count} in doc")
-		for letter_dict in letter_list:
-			character =  letter_dict['LETTER']
-			times = letter_dict['COUNT']
-			print(f"The {character} appears {times} times!")
-		print("--End Report--")
+	# Count Letters
+	word_count = len(words)
+	letter_count = {}
+	letter_list = []
+	for word in words: 
+		for letter in word:
+			if letter.isalpha():
+				if letter in letter_count:
+					letter_count[letter] += 1
+				else:
+					letter_count[letter] = 1
 
-	print_report()
+	# Letter counts to List of Dictionaries
+	for letter in letter_count:
+		letter_list.append({"LETTER": letter, "COUNT": letter_count[letter]})
 
-main()
+	
+	print(f"--Begin Report for {path}--")
+	print(f"There are {word_count} words in this book!")
+	for letter_dict in letter_list:
+		character =  letter_dict['LETTER']
+		times = letter_dict['COUNT']
+		print(f"The {character} character was found {times} times!")
+	print("--End Report--")
+
+main ()
+
+
